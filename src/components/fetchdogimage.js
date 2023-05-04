d0af7f8091236ad06d54ba761c6c1f24bed4e4fd
rishikeshkumar6 <rishikeshkumarsingh810@gmail.com>
2023-05-04 21:54:54 +0530

    this is my axios assignment

--- src/components/fetchdogimage.js
+++ src/components/fetchdogimage.js
@@ -1,20 +1,20 @@
 import React,{useState,useEffect} from 'react'
 import './fetchdogimage.css'
+import axios from 'axios'
 export  function FetchdogImage(){
     
     const [image,setimage]=useState('')
     useEffect(()=>{
     async function  Fetchdata(){
-    const request= await fetch("https://dog.ceo/api/breeds/image/random")
-    const response=await request.json()
-    setimage(response.message)
+    const response= await axios.get("https://dog.ceo/api/breeds/image/random")
+    setimage(response.data['message'])
     }
     Fetchdata()
     },[])
     return (
         <div>
            
-            <img src={image} height={'600px'} width={'700px'}/>
+            <img src={image} height={'600px'} width={'700px'} />
         </div>
     )
 }
